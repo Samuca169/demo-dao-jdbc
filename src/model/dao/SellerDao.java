5f554be29f76199bbb8e0d180d785f62ba96fe27
Samuca169 <samucabarr169@gmail.com>
2021-12-16 19:04:55 -0200

    DepartmentDao and SellerDao interfaces

---
+++ src/model/dao/SellerDao.java
@@ -0,0 +1,14 @@
+package model.dao;
+
+import java.util.List;
+
+import model.entites.Seller;
+
+public interface SellerDao {
+	
+	void insert(Seller obj);
+	void update(Seller obj);	
+	void deleteById(Integer Id);
+	Seller findByld(Integer Id);
+	List<Seller> fildAll();
+}
