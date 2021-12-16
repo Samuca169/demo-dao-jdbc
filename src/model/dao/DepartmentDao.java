fe382eb4e6fb6e6748cc240243b43bd563d835a5
Samuca169 <samucabarr169@gmail.com>
2021-12-16 19:15:22 -0200

    SellerDaoJDBC and DaoFactory

--- src/model/dao/DepartmentDao.java
+++ src/model/dao/DepartmentDao.java
@@ -1,5 +1,16 @@
 package model.dao;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import model.entites.Department;
+
 public interface DepartmentDao {
 
+	void insert(Department obj);
+	void update(Department obj);	
+	void deleteById(Integer Id);
+	Department findByld(Integer Id);
+	List<Department> fildAll();
 }
+
