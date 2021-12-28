a7d9d4a9bd7ef18ab7498c9d1731274a8edd7097
Samuca169 <samucabarr169@gmail.com>
2021-12-28 10:57:16 -0200

    update implementation

--- src/application/Program.java
+++ src/application/Program.java
@@ -35,5 +35,12 @@ public class Program {
 		Seller newSeller = new Seller(null, "Greg", "Greg@gmail.com", new Date(), 4000.0, department);
 		sellerDao.insert(newSeller);
 		System.out.println("Inserted! New id = " + newSeller.getId());
+		
+		System.out.println("\n=== TEST 5: seller update =====");
+		seller = sellerDao.findByld(1);
+		seller.setName("Marta Waine");
+		sellerDao.update(seller);
+		System.out.println("Update complete");
+
 	}
 }
